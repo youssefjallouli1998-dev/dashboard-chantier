99a2b01b4f39c39b00a7394580a883cae7a415d7
youssefjallouli1998-dev <youssefjallouli1998@gmail.com>
2026-03-25 09:53:23 +0000

    Remove misplaced next.config.js from pages/ folder

--- pages/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
-  },
-};
-
-module.exports = nextConfig;
